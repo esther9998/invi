52b44efec31a18e0d4135007ba9b256570a74b48
esther <maengester@172.20.10.2>
2019-10-01 01:18:59 +0900

    invi

---
+++ layout/nav.php
@@ -0,0 +1,41 @@
+<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-2">
+					<div id="fh5co-logo"><a href="index.php">Shop.</a></div>
+				</div>
+				<div class="col-md-6 col-xs-6 text-center menu-1">
+					<ul>
+						<li class="has-dropdown">
+							<a href="index.php">Home</a>
+						</li>
+						<li><a href="about.html">서비스</a></li>
+						<li class="has-dropdown">
+							<a href="services.html">초대장메이커</a>
+							<ul class="dropdown">
+								<li><a href="makingCard/index.php">웨딩</a></li>
+								<li><a href="#">돌잔치</a></li>
+								<li><a href="#">환갑</a></li>
+								<li><a href="#">기타</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+					<ul>
+						<li class="search">
+							<div class="input-group">
+						      <input type="text" placeholder="Search..">
+						      <span class="input-group-btn">
+						        <button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
+						      </span>
+						    </div>
+						</li>
+						<li class="shopping-cart"><a href="#" class="cart"><span><small>0</small><i class="icon-shopping-cart"></i></span></a></li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</nav>
---
+++ layout/scripts.php
@@ -0,0 +1,16 @@
+<!-- jQuery -->
+	<script src="../resources/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="../resources/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="../resources/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="../resources/js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="../resources/js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="../resources/js/jquery.countTo.js"></script>
+	<!-- Flexslider -->
+	<script src="../resources/js/jquery.flexslider-min.js"></script>
+	<!-- Main -->
+	<script src="../resources/js/main.js"></script>
